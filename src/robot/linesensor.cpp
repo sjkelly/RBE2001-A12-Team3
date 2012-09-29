8152e55d2ad994c8ba9cc8a4cb042c3415d7daa2
Gabriel Solomon Isko <gsisko@CCCWORK2.WPI.EDU>
2012-09-28 23:59:57 -0400

    fixed a bunch of line sensor errors

--- src/robot/linesensor.cpp
+++ src/robot/linesensor.cpp
@@ -23,23 +23,26 @@ uint16_t lineSensor::read()
     return micros() - time; 
   }
 }
-void lineSensor::updateAll()
+static void lineSensor::updateAll()
 {
  //charge all here with port assiment
  //LINE_PORT = CHARGE_VALUE;
  //these should both be defines
- pinMode(pin, OUTPUT);
- digitalWrite(pin, HIGH);
- //digitalWrite( QRE1113_Pin, HIGH );
- pinMode(pin, INPUT);
-}
-uint16_t lineSensor::readAll()
+  lineSensor *i;
+ for(i = this; i != NULL; i = i->next)
+  {
+   pinMode(i->getPin(), OUTPUT);
+   digitalWrite(i->getPin(), HIGH);
+   pinMode(i->getPin(), INPUT);
+   }
+ }
+static void lineSensor::readAll()
 {
  uitn32_t time = micros();
  while(micros - time < READ_MICROS)
  {
   lineSensor *i;
-  for(i = this; i != NULL; i = next)
+  for(i = this; i != NULL; i = i->next)
   {
    if(!digitalRead(i->getPin())&&!i->getValue())
     i->setValue(micros() - time);
@@ -54,12 +57,11 @@ void lineSensor::update()
   //Lower numbers mean more refleacive
   //More than 3000 means nothing was reflected.
   //pinMode( QRE1113_Pin, OUTPUT );
-  for(i = this; i != NULL; i = next)
-  {
-   pinMode(i->getPin(), OUTPUT);
-   digitalWrite(i->getPin(), HIGH);
-   pinMode(i->getPin(), INPUT);
-   }
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  //digitalWrite( QRE1113_Pin, HIGH );
+  pinMode(pin, INPUT);
+
    delayMicroseconds(CHARGE_MICROS);
   //pinMode( QRE1113_Pin, INPUT );
   lineSensor::readAll();
