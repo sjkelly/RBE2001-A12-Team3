517879e238a51a4237614eafe85a2dbe7aacf66d
Gabe Isko <gabestripes@gmail.com>
2012-10-07 21:43:16 -0400

    implemented the decision engine
    I also did some helper functions and enumerations and structures (mask,
    leading and preceding byte) that might be helpful elsewhere. We should
    consider moving them to robot.h

--- src/robot/decision.cpp
+++ src/robot/decision.cpp
@@ -1,4 +1,28 @@
 #include "decision.h"
+//some helper functions
+//tells the position of a leading bit in a mask
+uint8_t leadingByte(Mask _mask)
+{
+  uint8_t i;
+  for(i = 0;  i < 8; i++)
+  {
+    if(_mask.bitArray[i])
+     return i;
+  }
+  return 0;
+}
+
+//tells us the positin of the last bit in a mask
+uint8_t precedingByte(Mask _mask)
+{
+  uint8_t i;
+  for(i = 8;  i > 0; i--)
+  {
+    if(_mask.bitArray[i-1])
+     return i;
+  }
+  return 0;
+}
 
 decisionEng::decisionEng(fieldState *_state)
 { 
@@ -8,43 +32,91 @@ decisionEng::decisionEng(fieldState *_state)
 void decisionEng::supplyMessage(uint8_t _mask)
 {
  Mask spMask;
- spMask.mask = _mask;
+ spMask.Byte = _mask;
  int i = 0;
- state->new1 = spMask.bits[i];
+ state->storage.tubes.new1 = spMask.bitArray[i];
  i++;
- state->new2 = spMask.bits[i];
+ state->storage.tubes.new2 = spMask.bitArray[i];
  i++;
- state->new3 = spMask.bits[i];
+ state->storage.tubes.new3 = spMask.bitArray[i];
  i++;
- state->new4 = spMask.bits[i];
+ state->storage.tubes.new4 = spMask.bitArray[i];
 }
 
 void decisionEng::spentMessage(uint8_t _mask)
 {
  Mask spMask;
- spMask.mask = _mask;
+ spMask.Byte = _mask;
  int i = 0;
- state->spent1 = spMask.bits[i];
+ state->storage.tubes.spent1 = spMask.bitArray[i];
  i++;
- state->spent2 = spMask.bits[i];
+ state->storage.tubes.spent2 = spMask.bitArray[i];
  i++;
- state->spent3 = spMask.bits[i];
+ state->storage.tubes.spent3 = spMask.bitArray[i];
  i++;
- state->spent4 = spMask.bits[i];
+ state->storage.tubes.spent4 = spMask.bitArray[i];
  
 }
 
 uint8_t decisionEng::determineDest()
 {
+ Mask spentStorageState;
+ spentStorageState.Byte = state->storage.Byte & 0B11110000; 
+ Mask newStorageState;
+ newStorageState.Byte = state->storage.Byte & 0B00001111;
  switch (state->clawContents)
  {
-  case NO_ROD:
-   break;
+  
   case SPENT_ROD:
+   switch (leadingByte(spentStorageState))
+   {
+    case 1:
+    //return spent storage 1
+    break;
+    case 2:
+    //return spent storage 2
+    break;
+    case 3:
+    //return spent storage 3
+    break;
+    case 4:
+    //return spent storage 4
+    break;
+   } 
    break;
   case NEW_ROD:
+   if(state->reactorA == NO_ROD){}
+    //return reactor A
+   else{}
+    //return reactor B
    break;
+  case NO_ROD:
+   if ((state->reactorA == NO_ROD)||(state->reactorB == NO_ROD))
+    switch (precedingByte(newStorageState))
+    {
+     case 1:
+     //return new storage 1
+     break;
+     case 2:
+     //return new storage 2
+     break;
+     case 3:
+     //return new storage 3
+     break;
+     case 4:
+     //return new storage 4
+     break;
+    }
+   else if ((state->reactorA == SPENT_ROD)){}
+    //return reactor A
+   else if ((state->reactorB == SPENT_ROD)){}
+    //return reactor B
+   else{}
+   //this last case will only occur when we have finished all of our actions
+   //we can reset the field state here
  }
 }
 //these are some uncommited changes
 
+
+  
--- src/robot/decision.h
+++ src/robot/decision.h
@@ -1,21 +1,31 @@
+#ifndef ___DECISION_____
+#define ___DECISION_____
 #include <stdint.h>
 //enumeration for the contents of our claw
 enum tubeState {NO_ROD = 0x00, SPENT_ROD = 0x2C, NEW_ROD = 0xFF};
-//enum Action
+
 
 union Mask
 {
-  uint8_t mask;
-  bool bits[7];
+  uint8_t Byte;
+  bool bitArray[7];
+  struct{
+  bool bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7;
+  }bits;
+ //these are specifically here for the field state ussage of this
+  struct{
+  bool spent4, spent3, spent2, spent1, new1, new2, new3, new4;
+  }tubes;
 };
 
 //this is a struct so that we can access it outside of the class
 struct fieldState
 {
+ 
  //1 for available, 2 for occupied
- bool reactorA, spent4, spent3, spent2, spent1, reactorB, new1, new2, new3, new4;
+ Mask storage;
  //this represents the current content of our claw
- tubeState clawContents; 
+ tubeState clawContents, reactorA, reactorB;
 };
 
 class decisionEng
@@ -29,3 +39,5 @@ class decisionEng
   void spentMessage(uint8_t _mask);
   uint8_t determineDest();
 };
+
+#endif
